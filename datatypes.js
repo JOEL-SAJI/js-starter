6a856275aedaf65ec9760d4edd16a83da69da145
JOEL-SAJI <Joelsaji10a@gmail.com>
2024-07-06 12:42:12 +0000

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -1,2 +1,11 @@
 "use strict"// treal all js code as newer version
 //alert (3+3)// we are using node js not broweser
+//code readibility should be high
+let name="ratheesh"//string 
+let age=43//numeric
+let LoggedIn=true//boolean
+null // nothhing
+undefined // not defined
+//eg let biju=""
+console.log(typeof null) // object
+log
