2db05c155b9ec031768a8b99993aec25ee2aafc8
JOEL-SAJI <Joelsaji10a@gmail.com>
2024-07-06 12:52:34 +0000

    blaah

---
+++ basics/1_variables.js
@@ -0,0 +1,11 @@
+const accountId=1232;
+let accountName="babu";
+var accountEmail="babu@7615gmail.com"
+accountName="ramesh";
+accountEmail="ramesh7615@gmail.com"
+console.table([accountId,accountName,accountEmail])
+/*
+dont use var 
+because of block scope 
+dont be a pussy
+*/
